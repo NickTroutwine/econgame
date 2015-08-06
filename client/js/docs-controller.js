306f80a774551fb0e91b1f297392523be3bb667e
NickTroutwine <Nat88120@ucmo.edu>
2015-08-06 09:14:49 -0700

    logic down

--- client/js/docs-controller.js
+++ client/js/docs-controller.js
@@ -14,29 +14,36 @@ var self = this;
 	    	dataArray = data;
 		  	for(var i=0;i<dataArray.length;i++){
 		  		numArray.push(dataArray[i].numGuess);
-		  		console.log("numArray",numArray);
+		  		// console.log("numArray",numArray);
 		  	}
 		  	var sum = 0;
 		  	for (var i =0;i<numArray.length;i++){
 		  		sum += numArray[i]
 		  	}
 		    sum = Math.round((sum/data.length)/2);
-		  	console.log("sum",sum);
+		  	// console.log("sum",sum);
 		  	for(var i=0;i<numArray.length;i++){
 
 		  		ansArray.push(Math.abs(numArray[i]-sum));
-		  		console.log("ans",ansArray);
+		  		// console.log("ans",ansArray);
 		  	}
  				var answer = ansArray.indexOf(Math.min.apply(Math, ansArray));
- 				console.log("answer",answer);
- 				console.log(dataArray);
+ 				// console.log("answer",answer);
+ 				// console.log(dataArray);
 	    
 		  	for(var i=0;i<dataArray.length;i++){
 		  		if(i===answer){
-		  			var winner = dataArray[i];
-		  			console.log('winner',winner);
+		  			var winObj = dataArray[i];
+		  			var winnerName =(winObj.username);
+		  			var winnerGuess = (winObj.numGuess);
+		  			// console.log(winnerName, winnerGuess);
+		  			var players = dataArray.length
 		  		}
 		  	}
+		  	$scope.players = players;
+		  	$scope.sum = sum;
+		  	$scope.winnerName = winnerName;
+		  	$scope.winnerGuess = winnerGuess;
 		})
 		  			  	
 	}
