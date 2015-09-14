22d8629c3d81bc8cc16478383279df456ef68b65
MattEubanks <moosefan94@gmail.com>
2015-09-13 18:10:29 -0700

    Styled JS and HTML pages

--- client/js/docs-controller.js
+++ client/js/docs-controller.js
@@ -1,54 +1,39 @@
 angular.module('main').controller('DocsController', DocsController);
-DocsController.$inject = ['$http', '$scope']; 
+DocsController.$inject = ['$http', '$scope'];
 
-function DocsController($http, $scope) { 
-var self = this;
-// var dataArray;
-
-	this.getDocs  = function(){  
-	  var dataArray= [];  
-	  var numArray = [];
-	  var ansArray = [];
-	  var results;
-	  /*console.log(this.newUser)*/
-	    $http.get('/userguess').success(function(data, status, headers, config){
-	    	dataArray = data;
-		  	for(var i=0;i<dataArray.length;i++){
-		  		numArray.push(dataArray[i].numGuess);
-		  		// console.log("numArray",numArray);
-		  	}
-		  	var sum = 0;
-		  	for (var i =0;i<numArray.length;i++){
-		  		sum += numArray[i]
-		  	}
-		    sum = Math.round((sum/data.length)/2);
-		  	// console.log("sum",sum);
-		  	for(var i=0;i<numArray.length;i++){
-
-		  		ansArray.push(Math.abs(numArray[i]-sum));
-		  		// console.log("ans",ansArray);
-		  	}
- 				var answer = ansArray.indexOf(Math.min.apply(Math, ansArray));
- 				// console.log("answer",answer);
- 				// console.log(dataArray);
-	    
-		  	for(var i=0;i<dataArray.length;i++){
-		  		if(i===answer){
-		  			var winObj = dataArray[i];
-		  			var winnerName =(winObj.username);
-		  			var winnerGuess = (winObj.numGuess);
-		  			// console.log(winnerName, winnerGuess);
-		  			var players = dataArray.length
-		  		}
-		  	}
-		  	$scope.players = players;
-		  	$scope.sum = sum;
-		  	$scope.winnerName = winnerName;
-		  	$scope.winnerGuess = winnerGuess;
-		})
-		  			  	
-	}
+function DocsController($http, $scope) {
+  var self = this;
+  this.getDocs = function() {
+    var dataArray = [],
+      numArray = [],
+      ansArray = [],
+      results;
+    $http.get('/userguess').success(function(data, status, headers, config) {
+      dataArray = data;
+      for (var i = 0; i < dataArray.length; i++) {
+        numArray.push(dataArray[i].numGuess);
+      }
+      var sum = 0;
+      for (var i = 0; i < numArray.length; i++) {
+        sum += numArray[i]
+      }
+      sum = Math.round((sum / data.length) / 2);
+      for (var i = 0; i < numArray.length; i++) {
+        ansArray.push(Math.abs(numArray[i] - sum));
+      }
+      var answer = ansArray.indexOf(Math.min.apply(Math, ansArray));
+      for (var i = 0; i < dataArray.length; i++) {
+        if (i === answer) {
+          var winObj = dataArray[i],
+            winnerName = (winObj.username),
+            winnerGuess = (winObj.numGuess),
+            players = dataArray.length;
+        }
+      }
+      $scope.players = players;
+      $scope.sum = sum;
+      $scope.winnerName = winnerName;
+      $scope.winnerGuess = winnerGuess;
+    })
+  }
 }
-
-
-
--- client/js/main.js
+++ client/js/main.js
@@ -1,38 +1,37 @@
 (function() {
-angular.module('main', [
-  'ui.router'
-])
+  angular.module('main', [
+    'ui.router'
+  ])
 
-.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
-  $stateProvider
-    .state('home', {
-      url: '/',
-      templateUrl: 'pages/home.html',
-      controller: "NumController",
-      controllerAs: "numCtrl"
-    })
-    .state('results', {
-      url:'/results',
-      templateUrl:'pages/results.html',
-      controller: 'DocsController',
-      controllerAs: 'docsCtrl'
-    })
+  .config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'pages/home.html',
+        controller: "NumController",
+        controllerAs: "numCtrl"
+      })
+      .state('results', {
+        url: '/results',
+        templateUrl: 'pages/results.html',
+        controller: 'DocsController',
+        controllerAs: 'docsCtrl'
+      })
     $urlRouterProvider.otherwise('/');
     $locationProvider.html5Mode({
-       enabled: true
+      enabled: true
     });
   });
 })();
 
-window.addEventListener('load', function(){ 
+window.addEventListener('load', function() {
   var ws = new WebSocket('ws://localhost:8000');
-  ws.addEventListener('open', function(){
-        ws.send("whats up dawg");
-        console.log("message sent");
+  ws.addEventListener('open', function() {
+    ws.send("whats up dawg");
+    console.log("message sent");
   })
-    
-  ws.addEventListener('message',function(event){
-      console.log(event.data);
-    })
-})
 
+  ws.addEventListener('message', function(event) {
+    console.log(event.data);
+  })
+})
