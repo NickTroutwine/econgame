22d064ba191aa843055f1bc953e70cddcdcebf06
NickTroutwine <Nat88120@ucmo.edu>
2015-10-17 20:27:44 -0500

    connecting w/ console.logs

--- client/js/docs-controller.js
+++ client/js/docs-controller.js
@@ -11,41 +11,43 @@ function DocsController($http, $scope) {
       results;
 	  var ws = new WebSocket('ws://localhost:8000');
 	  ws.addEventListener('open', function() {
-	    ws.send('players');
+	    ws.send('you!');
 	    console.log("message sent");
 	  })
 
 	  ws.addEventListener('message', function(event) {
-	    console.log(event.data);
+	    console.log("player added");
+	    display();
 	  })
-
-    $http.get('/userguess').success(function(data, status, headers, config) {
-      dataArray = data;
-      for (var i = 0; i < dataArray.length; i++) {
-        numArray.push(dataArray[i].numGuess);
-      }
-      var sum = 0;
-      for (var i = 0; i < numArray.length; i++) {
-        sum += numArray[i]
-      }
-      sum = Math.round((sum / data.length) / 2);
-      for (var i = 0; i < numArray.length; i++) {
-        ansArray.push(Math.abs(numArray[i] - sum));
-      }
-      var answer = ansArray.indexOf(Math.min.apply(Math, ansArray));
-      for (var i = 0; i < dataArray.length; i++) {
-        if (i === answer) {
-          var winObj = dataArray[i],
-            winnerName = (winObj.username),
-            winnerGuess = (winObj.numGuess),
-            players = dataArray.length;
-        }
-      }
-      $scope.players = players;
-      $scope.sum = sum;
-      $scope.winnerName = winnerName;
-      $scope.winnerGuess = winnerGuess;
-    })
+	  function display (){
+	    $http.get('/userguess').success(function(data, status, headers, config) {
+	      dataArray = data;
+	      for (var i = 0; i < dataArray.length; i++) {
+	        numArray.push(dataArray[i].numGuess);
+	      }
+	      var sum = 0;
+	      for (var i = 0; i < numArray.length; i++) {
+	        sum += numArray[i]
+	      }
+	      sum = Math.round((sum / data.length) / 2);
+	      for (var i = 0; i < numArray.length; i++) {
+	        ansArray.push(Math.abs(numArray[i] - sum));
+	      }
+	      var answer = ansArray.indexOf(Math.min.apply(Math, ansArray));
+	      for (var i = 0; i < dataArray.length; i++) {
+	        if (i === answer) {
+	          var winObj = dataArray[i],
+	            winnerName = (winObj.username),
+	            winnerGuess = (winObj.numGuess),
+	            players = dataArray.length;
+	        }
+	      }
+	      $scope.players = players;
+	      $scope.sum = sum;
+	      $scope.winnerName = winnerName;
+	      $scope.winnerGuess = winnerGuess;
+	    })
+	  }  
   }
 }
 // window.addEventListener('load', function(){ 
